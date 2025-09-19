ffe69191b132f081afc28fcc5fb15b1d82edd1db
Lenin Mehedy <lenin.mehedy@hashgraph.com>
2025-09-19 13:06:17 +1000

    fix: temp code
    Signed-off-by: Lenin Mehedy <lenin.mehedy@hashgraph.com>

--- pkg/hardware/block_node.go
+++ pkg/hardware/block_node.go
@@ -1,1 +1,14 @@
 package hardware
+
+type blockNodeFull struct {
+	cpuMin  int64
+	cpuMax  int64
+	memMin  int64
+	memMax  int64
+	diskMin int64
+	diskMax int64
+}
+
+func (b *blockNodeFull) Cpu() int64 {
+
+}
