80b764a9f265bdbe5d3a950ab4d10bc5ccb82b8d
Bruno De Assis Marques <bruno.marques@swirldslabs.com>
2025-10-24 19:36:52 +1100

    feat: implement crio-o install and configure
    Signed-off-by: Bruno De Assis Marques <bruno.marques@swirldslabs.com>

--- internal/workflows/steps/step_crio.go
+++ internal/workflows/steps/step_crio.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/automa-saga/automa"
+	"golang.hedera.com/solo-provisioner/internal/workflows/notify"
 	"golang.hedera.com/solo-provisioner/pkg/software"
 )
 
@@ -17,6 +18,16 @@ func SetupCrio() automa.Builder {
 
 func installCrio(provider func(opts ...software.InstallerOption) (software.Software, error)) automa.Builder {
 	return automa.NewStepBuilder().WithId("install-crio").
+		WithPrepare(func(ctx context.Context, stp automa.Step) (context.Context, error) {
+			notify.As().StepStart(ctx, stp, "Setting up cri-o")
+			return ctx, nil
+		}).
+		WithOnFailure(func(ctx context.Context, stp automa.Step, rpt *automa.Report) {
+			notify.As().StepFailure(ctx, stp, rpt, "Failed to setup cri-o")
+		}).
+		WithOnCompletion(func(ctx context.Context, stp automa.Step, rpt *automa.Report) {
+			notify.As().StepCompletion(ctx, stp, rpt, "Cri-o setup successfully")
+		}).
 		WithExecute(func(ctx context.Context, stp automa.Step) *automa.Report {
 			installer, err := provider()
 			if err != nil {
@@ -54,6 +65,16 @@ func installCrio(provider func(opts ...software.InstallerOption) (software.Softw
 
 func configureCrio(provider func(opts ...software.InstallerOption) (software.Software, error)) automa.Builder {
 	return automa.NewStepBuilder().WithId("configure-crio").
+		WithPrepare(func(ctx context.Context, stp automa.Step) (context.Context, error) {
+			notify.As().StepStart(ctx, stp, "Configuring cri-o")
+			return ctx, nil
+		}).
+		WithOnFailure(func(ctx context.Context, stp automa.Step, rpt *automa.Report) {
+			notify.As().StepFailure(ctx, stp, rpt, "Failed to configure cri-o")
+		}).
+		WithOnCompletion(func(ctx context.Context, stp automa.Step, rpt *automa.Report) {
+			notify.As().StepCompletion(ctx, stp, rpt, "Cri-o configured successfully")
+		}).
 		WithExecute(func(ctx context.Context, stp automa.Step) *automa.Report {
 			installer, err := provider()
 			if err != nil {
