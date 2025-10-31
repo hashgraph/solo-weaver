9ccb859d88fc819a7119696ac32d56e4ce6bd4fc
Lenin Mehedy <lenin.mehedy@hashgraph.com>
2025-10-31 11:44:14 +1100

    feat: implement step to install metallb
    Signed-off-by: Lenin Mehedy <lenin.mehedy@hashgraph.com>
    
    # Conflicts:
    #	internal/workflows/steps/const.go
    
    # Conflicts:
    #	internal/workflows/steps/step_metallb.go

--- internal/workflows/steps/step_metallb.go
+++ internal/workflows/steps/step_metallb.go
@@ -4,17 +4,23 @@ import (
 	"context"
 
 	"github.com/automa-saga/automa"
+	"github.com/automa-saga/logx"
 	"golang.hedera.com/solo-provisioner/internal/workflows/notify"
+	"golang.hedera.com/solo-provisioner/pkg/helm"
 )
 
 const (
-	metaLBVersion = "0.15.2"
+	MetalLBNamespace = "metallb-system"
+	MetalLBRelease   = "metallb"
+	MetalLBChart     = "metallb/metallb"
+	MetalLBVersion   = "v0.13.10"
+	MetalLBRepo      = "https://metallb.github.io/metallb"
 )
 
 func SetupMetalLB() automa.Builder {
 	return automa.NewWorkflowBuilder().WithId("setup-metallb").Steps(
-		installMetalLB(metaLBVersion), // still using bash steps
-		configureMetalLB(),            // still using bash steps
+		installMetalLB(),
+		bashSteps.ConfigureMetalLB(),
 	).
 		WithPrepare(func(ctx context.Context, stp automa.Step) (context.Context, error) {
 			notify.As().StepStart(ctx, stp, "Setting up MetalLB")
@@ -25,5 +31,79 @@ func SetupMetalLB() automa.Builder {
 		}).
 		WithOnCompletion(func(ctx context.Context, stp automa.Step, rpt *automa.Report) {
 			notify.As().StepCompletion(ctx, stp, rpt, "MetalLB setup successfully")
+		})
+}
+
+func installMetalLB() automa.Builder {
+	return automa.NewStepBuilder().WithId("install-metallb").
+		WithExecute(func(ctx context.Context, stp automa.Step) *automa.Report {
+			l := logx.As()
+			hm, err := helm.NewManager(helm.WithLogger(*l))
+			if err != nil {
+				return automa.StepFailureReport(stp.Id(), automa.WithError(err))
+			}
+
+			meta := map[string]string{}
+			isInstalled, err := hm.IsInstalled(MetalLBRelease, MetalLBNamespace)
+			if err != nil {
+				return automa.StepFailureReport(stp.Id(), automa.WithError(err))
+			}
+
+			if isInstalled {
+				meta[AlreadyInstalled] = "true"
+				l.Info().Msg("MetalLB is already installed, skipping installation")
+				return automa.StepSuccessReport(stp.Id(), automa.WithMetadata(meta))
+			}
+
+			_, err = hm.AddRepo(MetalLBRelease, MetalLBRepo, helm.RepoAddOptions{})
+			if err != nil {
+				return automa.StepFailureReport(stp.Id(), automa.WithError(err))
+			}
+
+			_, err = hm.InstallChart(
+				ctx,
+				MetalLBRelease,
+				MetalLBChart,
+				MetalLBVersion,
+				MetalLBNamespace,
+				helm.InstallChartOptions{
+					CreateNamespace: true,
+				},
+			)
+			if err != nil {
+				return automa.StepFailureReport(stp.Id(), automa.WithError(err))
+			}
+
+			meta[InstalledByThisStep] = "true"
+			stp.State().Set(InstalledByThisStep, true)
+			return automa.StepSuccessReport(stp.Id(), automa.WithMetadata(meta))
+		}).
+		WithRollback(func(ctx context.Context, stp automa.Step) *automa.Report {
+			if stp.State().Bool(InstalledByThisStep) == false {
+				return automa.StepSkippedReport(stp.Id())
+			}
+
+			l := logx.As()
+			hm, err := helm.NewManager(helm.WithLogger(*l))
+			if err != nil {
+				return automa.StepFailureReport(stp.Id(), automa.WithError(err))
+			}
+
+			err = hm.UninstallChart(MetalLBRelease, MetalLBNamespace)
+			if err != nil {
+				return automa.StepFailureReport(stp.Id(), automa.WithError(err))
+			}
+
+			return automa.StepSuccessReport(stp.Id())
+		}).
+		WithPrepare(func(ctx context.Context, stp automa.Step) (context.Context, error) {
+			notify.As().StepStart(ctx, stp, "Installing MetalLB")
+			return ctx, nil
+		}).
+		WithOnFailure(func(ctx context.Context, stp automa.Step, rpt *automa.Report) {
+			notify.As().StepFailure(ctx, stp, rpt, "Failed to install MetalLB")
+		}).
+		WithOnCompletion(func(ctx context.Context, stp automa.Step, rpt *automa.Report) {
+			notify.As().StepCompletion(ctx, stp, rpt, "MetalLB installed successfully")
 		})
 }
