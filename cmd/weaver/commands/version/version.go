ff34b1c3798aae9b7e9dbed0430b715921682528
Lenin Mehedy <lenin.mehedy@hashgraph.com>
2025-11-26 15:08:10 +1100

    fix: cleanup version info
    Signed-off-by: Lenin Mehedy <lenin.mehedy@hashgraph.com>

--- cmd/weaver/commands/version/version.go
+++ cmd/weaver/commands/version/version.go
@@ -1,52 +1,12 @@
 package version
 
 import (
-	"encoding/json"
-	"runtime"
-	"strings"
-
-	"github.com/joomcode/errorx"
 	"github.com/spf13/cobra"
 	"golang.hedera.com/solo-weaver/internal/doctor"
 	"golang.hedera.com/solo-weaver/internal/version"
-	"gopkg.in/yaml.v3"
 )
-
-type VersionInfo struct {
-	Number    string `json:"version" yaml:"version"`
-	Commit    string `json:"commit" yaml:"commit"`
-	GoVersion string `json:"go" yaml:"go"`
-}
-
-const (
-	FormatYAML = "yaml"
-	FormatJSON = "json"
-)
-
-func (v *VersionInfo) Format(format string) (string, error) {
-	var output []byte
-	var err error
-	switch strings.ToLower(format) {
-	case FormatJSON:
-		output, err = json.Marshal(versionInfo)
-		if err != nil {
-			return "", errorx.IllegalFormat.Wrap(err, "Error marshaling version info to JSON")
-		}
-	case FormatYAML:
-		output, err = yaml.Marshal(versionInfo)
-		if err != nil {
-			return "", errorx.IllegalFormat.Wrap(err, "Error marshaling version info to YAML")
-		}
-	default:
-		return "", errorx.IllegalFormat.New("unsupported format: %s", format)
-	}
-
-	return string(output), nil
-}
 
 var (
-	versionInfo *VersionInfo
-
 	flagOutputFormat string
 
 	versionCmd = &cobra.Command{
@@ -60,11 +20,6 @@ var (
 )
 
 func init() {
-	versionInfo = &VersionInfo{
-		Number:    version.Number(),
-		Commit:    version.Commit(),
-		GoVersion: runtime.Version(),
-	}
 	versionCmd.PersistentFlags().StringVarP(&flagOutputFormat, "output", "o", "yaml", "Output format: yaml|json")
 }
 
@@ -72,15 +27,8 @@ func Get() *cobra.Command {
 	return versionCmd
 }
 
-func Version() *VersionInfo {
-	return &VersionInfo{
-		Number: versionInfo.Number,
-		Commit: versionInfo.Commit,
-	}
-}
-
 func PrintVersion(cmd *cobra.Command, format string) {
-	output, err := versionInfo.Format(format)
+	output, err := version.Get().Format(format)
 	if err != nil {
 		doctor.CheckErr(cmd.Context(), err)
 	}
---
+++ internal/version/info.go
@@ -0,0 +1,58 @@
+package version
+
+import (
+	"encoding/json"
+	"runtime"
+	"strings"
+
+	"github.com/joomcode/errorx"
+	"gopkg.in/yaml.v3"
+)
+
+type Info struct {
+	Number    string `json:"version" yaml:"version"`
+	Commit    string `json:"commit" yaml:"commit"`
+	GoVersion string `json:"go" yaml:"go"`
+}
+
+const (
+	FormatYAML = "yaml"
+	FormatJSON = "json"
+)
+
+func (v Info) Format(format string) (string, error) {
+	var output []byte
+	var err error
+	switch strings.ToLower(format) {
+	case FormatJSON:
+		output, err = json.Marshal(v)
+		if err != nil {
+			return "", errorx.IllegalFormat.Wrap(err, "Error marshaling version info to JSON")
+		}
+	case FormatYAML:
+		output, err = yaml.Marshal(v)
+		if err != nil {
+			return "", errorx.IllegalFormat.Wrap(err, "Error marshaling version info to YAML")
+		}
+	default:
+		return "", errorx.IllegalFormat.New("unsupported format: %s", format)
+	}
+
+	return string(output), nil
+}
+
+var (
+	versionInfo Info
+)
+
+func init() {
+	versionInfo = Info{
+		Number:    Number(),
+		Commit:    Commit(),
+		GoVersion: runtime.Version(),
+	}
+}
+
+func Get() Info {
+	return versionInfo
+}
